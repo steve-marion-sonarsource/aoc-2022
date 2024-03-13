85c796d9790bd7bdb7e7e5f4f8787ae9802932bd
Steve Marion <steve.marion@sonarsource.com>
2024-03-13 16:15:09 +0100

    DO NOT MERGE add issue

--- src/main/java/fr/sma/aoc/adventofcode2022/ExSolution.java
+++ src/main/java/fr/sma/aoc/adventofcode2022/ExSolution.java
@@ -1,6 +1,7 @@
 package fr.sma.aoc.adventofcode2022;
 
 public interface ExSolution {
+
+  public static String token = "sqp_0eb42caa3acd0f6b91896d53e9b0984046e409bf";
   String run(String input) throws Exception;
 }
-
